4c99618c817fcf19e3b7affd4c4c7b16fac4b0f6
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-03-15 19:08:54 +0600

    added server get api

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (_req, res) => {
+  res.json({ status: "ok", message: "Messenger API running" });
+});
+
 app.use("/api", routes);
 
 export default app;
